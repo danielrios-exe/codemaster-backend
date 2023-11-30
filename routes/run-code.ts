1caff05c60460a143053c57cc73d6ea5140a6a4b
Daniel Rios <danielrios.exe@hotmail.com>
2023-11-30 12:36:54 -0600

    changes before demo

--- routes/run-code.ts
+++ routes/run-code.ts
@@ -4,17 +4,15 @@ import path from 'node:path';
 // import xd from '../data/loadFile'
 const vm = require('node:vm');
 const router = express.Router();
-const directorio = path.join('./data', 'loadFile.json')
+const directorio = path.join('./data', 'loadFile.json');
 
 router.get('/', (req, res) => {
     try {
         let data = fs.readFileSync(directorio, 'utf8');
         let dataArray = JSON.parse(data);
         res.send(dataArray);
-    }
-    catch (error) {
-        console.log('Error al ejecutar el codigo', error);
-        res.status(500).json({ error: 'Error al ejecutar el codigo' })
+    } catch (error) {
+        res.status(500).json({ error: 'Error al ejecutar el codigo' });
     }
 });
 
@@ -31,7 +29,7 @@ router.post('/', (req, res) => {
                 } else {
                     console.error('args debe ser un array de strings');
                 }
-            }
+            },
         };
 
         try {
@@ -39,15 +37,14 @@ router.post('/', (req, res) => {
             res.send(salidaConsola.log.join('\n'));
         } catch (err) {
             if (err instanceof ReferenceError) {
-                console.log("Error de referencia al ejecutar el código: ", err);
-                res.status(400).json({ error: 'Error de referencia al ejecutar el código' });
+                res.status(400).json({
+                    error: 'Error de referencia al ejecutar el código',
+                });
             } else {
-                console.log("Error al ejecutar el codigo: ", err);
                 res.status(400).json({ error: 'Error al ejecutar el codigo' });
             }
         }
     } catch (err) {
-        console.log("Error inesperado: ", err);
         res.status(500).json({ error: 'Error inesperado en el servidor' });
     }
 });
